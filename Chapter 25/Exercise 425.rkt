0d887e0732a66328e16604d58b4d57af7f7378f0
Kamry Bowman <kamry.bowman@gmail.com>
2018-12-11 16:38:48 -0700

    refactor to divide lists in single recursion

--- Chapter 25/Exercise 425.rkt
+++ Chapter 25/Exercise 425.rkt
@@ -7,29 +7,25 @@
   (cond
     [(empty? alon) '()]
     [(empty? (rest alon)) alon]
-    [else (local ((define pivot (first alon)))
-                  (append (quick-sort< (smallers (rest alon) pivot))
-                          (list pivot)
-                          (quick-sort< (largers(rest alon) pivot))))]))
+    [else (local ((define pivot (first alon))
+                  (define (pivot-check n)
+                    (> n pivot))
+                  (define small-large (split (rest alon) pivot-check)))
+            (append (quick-sort< (first small-large))
+                    (list pivot)
+                    (quick-sort< (second small-large))))]))
 
 (check-expect (quick-sort< '(3 4 9 1 8 7 6)) '(1 3 4 6 7 8 9))
 
-; [List-of Number] Number -> [List-of Number]
-; Returns a list that includes all numbers smaller than pivot
-(define (smallers lon pivot)
-  (cond [(empty? lon) '()]
-        [(< (first lon) pivot) (cons (first lon) (smallers (rest lon) pivot))]
-        [else (smallers (rest lon) pivot)]))
-
-(check-expect (smallers '(3 2 7 8 9 3) 5) '(3 2 3))
-
-
-; [List-of Number] Number -> [List-of Number]
-; Returns a list that includes all numbers equal to or greater than pivot
-(define (largers lon pivot)
-  (cond [(empty? lon) '()]
-        [(>= (first lon) pivot) (cons (first lon) (largers (rest lon) pivot))]
-        [else (largers (rest lon) pivot)]))
-
-(check-expect (largers '(3 2 7 8 9 3) 5) '(7 8 9))
+; [List-of X] [X->Boolean] -> '(,[List-of X] ,[List-of X])
+; Returns a list of two lists, with all X that evaluate as false in first list,
+; and all that evaluate as true in the second
+(define (split lon predicate)
+  (local ((define (splitter lon false-l true-l)
+            (cond [(empty? lon) `(,false-l ,true-l)]
+                  [(predicate (first lon)) (splitter (rest lon) false-l (cons (first lon) true-l))]
+                  [else (splitter (rest lon) (cons (first lon) false-l) true-l)])))
+    (splitter lon '() '())))
+    
+  
 
