524f36a105fa3f7d0090b07f6e578a46a235f951
Kamry Bowman <kamry.bowman@gmail.com>
2018-12-15 12:04:16 -0700

    Implement slope

---
+++ Chapter 27/Exercise 455.rkt
@@ -0,0 +1,32 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname |Exercise 455|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define epsilon .01)
+
+; [Number -> Number] Number -> Number
+; Consumes a function and an x value, and
+; returns the slope of the line through
+; `(,(- r1 epsilon) ,(f (- r1 epsilon)))
+; and `(,(+ r1 epsilon) ,(f (+ r1 epsilon)))
+(define (slope f r1)
+  (local ((define r1- (- r1 epsilon))
+          (define f@r1- (f r1-))
+          (define r1+ (+ r1 epsilon))
+          (define f@r1+ (f r1+)))
+    (/ (- f@r1+ f@r1-)
+       (- r1+ r1-))))
+
+(define (linear1 x)
+  (- 5 (* 2 x)))
+(check-expect (slope linear1 2) -2)
+
+(define (linear2 x)
+  (+ 6 (* 3 x)))
+(check-expect (slope linear2 3) 3)
+
+(define (poly x)
+  (* 3 (expt x 2)))
+(check-expect (slope poly 1) 6)
+
+
+  
