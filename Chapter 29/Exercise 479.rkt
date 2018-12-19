9dbff9be39f3ca38a4237a7bd006a282a1bf5f98
Kamry Bowman <kamry.bowman@gmail.com>
2018-12-18 17:43:09 -0700

    N-Queens problem solution

--- Chapter 29/Exercise 479.rkt
+++ Chapter 29/Exercise 479.rkt
@@ -81,8 +81,108 @@
                                    (draw-queens (rest loq) img)))])))
     (draw-queens loq (render-board n WIDTH))))
 
-(render-queens 8 `(,q00 ,q44 ,q57 ,q22 ,q63))
 
 
-                            
+
+; A Board is a structure
+(define-struct board [n unthreat occupied])
+; - n represents the number of rows on the board
+; - unthreatened is a [List-of posn] representing unthreatened positions
+; - occupied is a [List-of QP]
+
+; N -> Board 
+; creates the initial n by n board
+(check-expect (board0 1) (make-board 1 `(,(make-posn 0 0)) '()))
+(check-expect (board0 2) (make-board 2 `(,(make-posn 1 1)
+                                         ,(make-posn 0 1)
+                                         ,(make-posn 1 0)
+                                         ,(make-posn 0 0)) '()))
+(define (board0 n)
+  (local ((define target (* n n))
+          (define (fill-board current lop)
+            (cond
+              [(= current target) lop]
+              [else (local ((define x (modulo current n))
+                            (define y (quotient current n))
+                            (define pos (make-posn x y)))
+                      (fill-board (add1 current) (cons pos lop)))]))
+          (define unthreat (fill-board 0 '())))
+  (make-board n unthreat '())))
+
+; Board QP -> Board 
+; places a queen at qp on a-board
+(define (add-queen a-board qp)
+  (local ((define (update-threat lop new)
+            (filter (lambda (p) (not (threatening? new p))) lop))
+          (define unthreat (board-unthreat a-board))
+          (define n (board-n a-board))
+          (define occupied (board-occupied a-board)))
+    (make-board n (update-threat unthreat qp) (cons qp occupied))))
+
+(define empty-board
+  (make-board 3 `(,(make-posn 0 0) ,(make-posn 0 1) ,(make-posn 0 2)
+                                   ,(make-posn 1 0) ,(make-posn 1 1) ,(make-posn 1 2)
+                                   ,(make-posn 2 0) ,(make-posn 2 1) ,(make-posn 2 2))
+              '()))
+(define queen-corner
+  (make-board 3 `(,(make-posn 1 2) ,(make-posn 2 1)) `(,(make-posn 0 0))))
+(check-expect (add-queen empty-board (make-posn 0 0)) queen-corner)
+                                  
+                                
+
+; Board -> [List-of QP]
+; finds spots where it is still safe to place a queen
+(define (find-open-spots a-board)
+  (board-unthreat a-board))
+
+; N -> [[List-of QP] -> Boolean]
+(define (n-queens-solution? n)
+  (local ((define (process-q loq)
+            (cond
+              [(empty? loq) #true]
+              [else (if (ormap (lambda (q2) (threatening? (first loq) q2)) (rest loq))
+                        #false
+                        (process-q (rest loq)))]))
+          (define (pred loq)
+            (if (= (length loq) n)
+                (process-q loq)
+                #false)))
+    pred))
+
+(define q20 (make-posn 2 0))
+(define q01 (make-posn 0 1))
+(define q32 (make-posn 3 2))
+(define q13 (make-posn 1 3))
+
+(define 4-good (list q20 q01 q32 q13))
+(define 4-good-rev (list q13 q32 q01 q20))
+
+(define q23 (make-posn 2 3))
+(define 4-bad (list q20 q01 q32 q23))
+
+(define check-4 (n-queens-solution? 4))
+(check-satisfied 4-good check-4)
+(check-satisfied 4-good-rev check-4)
+(check-expect (check-4 4-bad) #false)
+
+; Board N -> [Maybe [List-of QP]]
+; places n queens on board; otherwise, returns #false
+(define (place-queens a-board n)
+  (cond
+    [(= n 0) a-board]
+    [else (local ((define open-spots (find-open-spots a-board))
+                  (define (process-spots los)
+                    (cond
+                      [(empty? los) #false]
+                      [else (local ((define one-placement (add-queen a-board (first los)))
+                                    (define candidate (place-queens one-placement (sub1 n))))
+                              (if (boolean? candidate)
+                                  (process-spots (rest los))
+                                  candidate))])))
+            (process-spots open-spots))]))
+(check-satisfied (board-occupied (place-queens (board0 4) 4)) check-4)
+
+(render-queens 8 (board-occupied (place-queens (board0 8) 8)))
+
+
               
